a5235b71d87b8a45b4afc515ad5bb693849d043f
sebastian.badon <sebastian.badon@velir.com>
2018-02-22 15:54:30 -0500

    RWJF-13243 Adding new selector to set bound based on width and height to ideal dimension

--- source/core/src/main/java/com/aem/toolbox/servlet/image/ImageSizeProperty.java
+++ source/core/src/main/java/com/aem/toolbox/servlet/image/ImageSizeProperty.java
@@ -1,6 +1,5 @@
 package com.aem.toolbox.servlet.image;
 
-import java.awt.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,7 +11,9 @@ import java.util.regex.Pattern;
  */
 public class ImageSizeProperty {
 	private static final String REGEX = "(\\d+)([x_])(\\d+)";
+	private static final String SINGLE_DIMENSION_REGEX = "(\\d+)";
 	private static final Pattern PATTERN = Pattern.compile(REGEX);
+	private static final Pattern SINGLE_WIDTH_DIMENSION_PATTERN= Pattern.compile(SINGLE_DIMENSION_REGEX);
 	private final Dimension dimension;
 	private final String property;
 	private final boolean legacyAspectRatio;
@@ -23,11 +24,19 @@ public class ImageSizeProperty {
 		}
 		Matcher matcher = PATTERN.matcher(input);
 		return matcher.matches() && ! matcher.group(1).equals("0") && ! matcher.group(3).equals("0")
-			? matcher : null;
+				? matcher : null;
+	}
+
+	private static Matcher tryToMatchSingleWidthDimension(String input) {
+		if (input == null) {
+			return null;
+		}
+		Matcher matcher = SINGLE_WIDTH_DIMENSION_PATTERN.matcher(input);
+		return matcher.matches() && ! matcher.group(1).equals("0") ? matcher : null;
 	}
 
 	public static boolean isValid(String input) {
-		return tryToMatch(input) != null;
+		return tryToMatch(input) != null || tryToMatchSingleWidthDimension(input) != null;
 	}
 
 	public static ImageSizeProperty parse(String imageSize) {
@@ -41,6 +50,19 @@ public class ImageSizeProperty {
 		int parsedHeight = Integer.parseInt(matcher.group(3));
 
 		return new ImageSizeProperty(new Dimension(parsedWidth, parsedHeight), imageSize, legacyAspectRatio);
+	}
+
+	public static ImageSizeProperty parseSingleWidthDimension(String imageSize) {
+		Matcher matcher = tryToMatchSingleWidthDimension(imageSize);
+		if (matcher == null) {
+			throw new IllegalArgumentException(String.format("The string %s doesn't match the pattern %s", "" + imageSize, SINGLE_WIDTH_DIMENSION_PATTERN));
+		}
+
+		int parsedWidth = Integer.parseInt(matcher.group(1));
+		boolean legacyAspectRatio = false;
+		int height = Integer.MAX_VALUE;
+
+		return new ImageSizeProperty(new Dimension(parsedWidth, height), imageSize, legacyAspectRatio);
 	}
 
 	protected ImageSizeProperty(Dimension dimension, String property, boolean legacyAspectRatio) {
